ffef06df863aa69f5edc9183e355a69b013d4e1d
Morten <simply.cod.92@gmail.com>
2026-02-23 10:54:08 +0100

    scroll now centers
    scrolling half page now centers the cursor on screen

--- src/motions.c
+++ src/motions.c
@@ -13,6 +13,8 @@ void motion_scrollHalfPageDown(ViewPort *view, Buffer *buff, BufferInfo *info) {
         buff->current = buff->current->next;
         info->currentLineNumber++;
     }
+    if (info->currentLineNumber > view->height / 2)
+        view->topLine = info->currentLineNumber - (view->height / 2);
 }
 
 void motion_scrollHalfPageUp(ViewPort *view, Buffer *buff, BufferInfo *info) {
@@ -25,4 +27,6 @@ void motion_scrollHalfPageUp(ViewPort *view, Buffer *buff, BufferInfo *info) {
         buff->current = buff->current->previous;
         info->currentLineNumber--;
     }
+    if (info->currentLineNumber > view->height / 2)
+        view->topLine = info->currentLineNumber - (view->height / 2);
 }
